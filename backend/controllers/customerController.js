240619025b11b526467a29d08a19fbb31413369b
Zane <ericatkusa@gmail.com>
2024-07-21 14:35:33 +0530

    Customer Controller

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -1,5 +1,3 @@
-// customerController.js
-
 const Customer = require('../models/Customer');
 
 exports.createOrUpdateCustomer = async (req, res) => {
@@ -14,7 +12,7 @@ exports.createOrUpdateCustomer = async (req, res) => {
         let customer = await Customer.findOne({ name: formattedName });
 
         if (customer) {
-            res.send(customer);
+            return res.status(400).json({ message: 'Customer already exists. Please edit the existing customer.' });
         } else {
             const lastCustomer = await Customer.findOne().sort({ customerId: -1 });
             const newCustomerId = lastCustomer ? lastCustomer.customerId + 1 : 1;
@@ -27,28 +25,43 @@ exports.createOrUpdateCustomer = async (req, res) => {
             });
 
             await customer.save();
-            res.send(customer);
+            res.status(201).json(customer);
         }
     } catch (err) {
         console.error(err);
-        res.status(500).send({ message: "Server error", error: err });
+        res.status(500).json({ message: "Server error", error: err });
     }
 };
 
+exports.getCustomers = async (req, res) => {
+    try {
+        const customers = await Customer.find();
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
+exports.updateCustomer = async (req, res) => {
+    try {
+        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!customer) {
+            return res.status(404).json({ message: 'Customer not found' });
+        }
+        res.json(customer);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
 
-
-        exports.getCustomers = async (req, res) => {
-            const customers = await Customer.find();
-            res.send(customers);
-        };
-
-        exports.updateCustomer = async (req, res) => {
-            const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true });
-            res.send(customer);
-        };
-
-        exports.deleteCustomer = async (req, res) => {
-            await Customer.findByIdAndDelete(req.params.id);
-            res.send({ message: 'Customer deleted' });
-        };
+exports.deleteCustomer = async (req, res) => {
+    try {
+        const customer = await Customer.findByIdAndDelete(req.params.id);
+        if (!customer) {
+            return res.status(404).json({ message: 'Customer not found' });
+        }
+        res.json({ message: 'Customer deleted' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
